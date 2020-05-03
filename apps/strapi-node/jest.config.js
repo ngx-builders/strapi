3ec4f97d68c4b0e81f22ede95e03aba479cd1358
Ash Brener <ashley@starlogik.com>
2020-05-03 14:19:03 +0200

    Revert "Merge pull request #15 from ngx-builders/renovate/node-8.x"

--- apps/strapi-node/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  name: 'strapi',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/strapi'
-};
--- apps/strapi-node/src/strapi-typescript/register.js
+++
@@ -1,9 +0,0 @@
-require('ts-node/register');
-require('tsconfig-paths/register');
-const { join } = require('path');
-const moduleAlias = require('module-alias')
-
-moduleAlias.addAliases({
-  './load-apis': join(__dirname, 'load-apis'),
-  './filepath-to-prop-path': join(__dirname, 'filepath-to-prop-path')
-})
