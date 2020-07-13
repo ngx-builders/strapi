d3244bce92bb93623f408316036ec6e99806be2e
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-07-13 21:50:24 +0530

    fix: controller

--- apps/strapi-node/src/api/custom/controllers/custom.ts
+++ apps/strapi-node/src/api/custom/controllers/custom.ts
@@ -1,7 +1,6 @@
 import { Context } from 'koa';
-import { greeting } from '@ngx-builders/ts-example';
 
 export async function ts(ctx: Context) {
   ctx.set({ 'Content-Type': 'text/html' });
-  return greeting();
+  return 'Hello World!';
 }
